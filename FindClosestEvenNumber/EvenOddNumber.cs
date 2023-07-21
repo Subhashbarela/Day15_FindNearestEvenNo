ac03910a2f476dcb2f2c58b31d848b189d415892
Subhash Barela <barelasubhash16@gmail.com>
2023-07-21 14:10:59 +0530

    [subhash] Find Closest Even number

---
+++ FindClosestEvenNumber/EvenOddNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindClosestEvenNumber
+{
+    public class EvenOddNumber
+    {
+        public void FindEven()
+        {
+            Console.WriteLine("Enter the number that you want find Closest Even");
+            int Murli=int.Parse(Console.ReadLine());
+            if(Murli%2==0 )
+            {
+                Console.WriteLine(Murli);
+            }
+            else
+            {
+                Console.WriteLine(Murli+1);
+            }
+        }
+    }
+}
---
+++ FindClosestEvenNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindClosestEvenNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenOddNumber evenOdd=new EvenOddNumber();
+            evenOdd.FindEven();
+        }
+    }
+}
