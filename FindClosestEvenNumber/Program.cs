aca90177f3e8241763a4629e08d366e2fce7e1f6
Subhash Barela <barelasubhash16@gmail.com>
2023-07-21 14:45:59 +0530

    [Subhash] Create Reflection And Fetch Type Info

--- FindClosestEvenNumber/Program.cs
+++ FindClosestEvenNumber/Program.cs
@@ -10,9 +10,32 @@ namespace FindClosestEvenNumber
     {
         static void Main(string[] args)
         {
-            EvenOddNumber evenOdd=new EvenOddNumber();
-            int[] arr = { 8,10, 12,16, 18, 22, 24, 26, 28, };
-            evenOdd.FindEven(arr);
+            Console.WriteLine("1:Find Closest Even \n2: Describe type Infomartion");
+            Console.WriteLine("Enter the choice");
+            int choice = int.Parse(Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    {
+                        EvenOddNumber evenOdd = new EvenOddNumber();
+                        int[] arr = { 8, 10, 12, 16, 18, 22, 24, 26, 28, };
+                        evenOdd.FindEven(arr);
+                        break;
+                    }
+                case 2:
+                    {
+                        ReflectionClass reflection = new ReflectionClass();
+                        reflection.FetchDataOfEvenClass();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Please insert valid input");
+                        break;
+                    }
+            }
+
+
         }
     }
 }
---
+++ FindClosestEvenNumber/ReflectionClass.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FindClosestEvenNumber
+{
+    public class ReflectionClass
+    {
+        public void FetchDataOfEvenClass()
+        {      
+
+            Type type = typeof(EvenOddNumber);
+            Console.WriteLine("Class Name is : "+type.Name);
+            Console.WriteLine("Full Name is : "+type.FullName);
+            Console.WriteLine("NameSpace  is : "+type.Namespace);
+            MethodInfo[] m= type.GetMethods();
+            foreach(MethodInfo m2 in m)
+            {
+                Console.WriteLine("Return Type: "+m2.ReturnType+" Attributs: "+m2.Attributes);
+            }
+        }
+    }
+}
